fe194ddf6772c33ea054ebd475d5039ea8561098
jooyb0831 <dia231@naver.com>
2025-02-24 15:49:33 +0900

    0224

--- Assets/0.Scripts/BoardManager.cs
+++ Assets/0.Scripts/BoardManager.cs
@@ -13,8 +13,14 @@ public class BoardManager : MonoBehaviour
     //셀 데이터 받아오는 2차원 배열
     private CellData[,] m_BoardData;
 
+    //플레이어
+    public PlayerController Player;
+
     //타일맵 컴포넌트
     private Tilemap m_Tilemap;
+
+    //그리드 컴포넌트
+    private Grid m_Grid;
 
     //생성할 타일맵의 크기
     public int Width;
@@ -24,13 +30,16 @@ public class BoardManager : MonoBehaviour
     public Tile[] GroundTiles; //바닥
     public Tile[] WallTiles; //벽
 
+    
+
     // Start is called before the first frame update
     void Start()
     {
         //타일맵 컴포넌트 받아오기
         m_Tilemap = GetComponentInChildren<Tilemap>();
+        m_Grid = GetComponent<Grid>();
 
-        //보드 데이터 설정
+        //보드 데이터 설정 (전체 새로 지칭)
         m_BoardData = new CellData[Width, Height];
 
         //높이만큼 돌림
@@ -40,7 +49,10 @@ public class BoardManager : MonoBehaviour
             for (int x = 0; x < Width; x++)
             {
                 Tile tile;
+
+                 //각각의 셀 데이터는 따로 만들어줘야 함
                 m_BoardData[x, y] = new CellData();
+
                 //가장자리일 경우
                 if (x == 0 || y == 0 || x == Width - 1 || y == Height - 1)
                 {
@@ -60,8 +72,34 @@ public class BoardManager : MonoBehaviour
                 m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
             }
         }
+        //플레이어 스폰
+        Player.Spawn(this, new Vector2Int(1, 1));
     }
 
+
+    /// <summary>
+    /// cell인덱스를 Grid의 World포지로 변경
+    /// </summary>
+    /// <param name="cellIndex"></param>
+    /// <returns></returns>
+    public Vector3 CellToWorld(Vector2Int cellIndex)
+    {
+        return m_Grid.GetCellCenterWorld((Vector3Int)cellIndex);
+    }
+
+
+    public CellData GetCellData(Vector2Int cellIndex)
+    {
+        //범위체크(범위 안 벗어나게끔)
+        if (cellIndex.x < 0 || cellIndex.x >= Width
+        || cellIndex.y < 0 || cellIndex.y >= Height)
+        {
+            return null;
+        }
+
+        //셀 인덱스 값 리턴
+        return m_BoardData[cellIndex.x, cellIndex.y];
+    }
     // Update is called once per frame
     void Update()
     {
--- Assets/0.Scripts/PlayerController.cs
+++ Assets/0.Scripts/PlayerController.cs
@@ -1,9 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private BoardManager m_Board;
+    private Vector2Int m_CellPosition;
+
+
+    /// <summary>
+    /// 플레이어를 보드에 스폰하는 코드
+    /// </summary>
+    /// <param name="boardManager"></param>
+    /// <param name="cell"></param>
+    public void Spawn(BoardManager boardManager, Vector2Int cell)
+    {
+        //변수 받아서 지정
+        m_Board = boardManager;
+        m_CellPosition = cell;
+
+        //위치선정
+        MoveTo(cell);
+    }
+
+
+    /// <summary>
+    /// 셀 위치 설정 및 이동
+    /// </summary>
+    /// <param name="cell"></param>
+    public void MoveTo(Vector2Int cell)
+    {
+        m_CellPosition = cell;
+        transform.position = m_Board.CellToWorld(m_CellPosition);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +44,46 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2Int newCellTarget = m_CellPosition;
+        bool hasMoved = false;
+
+        //기존의 InputManager가 아닌 새로운 InputSystem을 활용
+        if (Keyboard.current.upArrowKey.wasPressedThisFrame)
+        {
+            newCellTarget.y++;
+            hasMoved = true;
+        }
+        else if (Keyboard.current.downArrowKey.wasPressedThisFrame)
+        {
+            newCellTarget.y--;
+            hasMoved = true;
+        }
+        else if (Keyboard.current.rightArrowKey.wasPressedThisFrame)
+        {
+            newCellTarget.x++;
+            hasMoved = true;
+        }
+        else if (Keyboard.current.leftArrowKey.wasPressedThisFrame)
+        {
+            newCellTarget.x--;
+            hasMoved = true;
+        }
+
+        if (hasMoved)
+        {
+            //움직이려는 셀의 데이터 가져오기
+            BoardManager.CellData cellData = m_Board.GetCellData(newCellTarget);
+
+            //셀데이터가 Null이 아니고, 이동할 수 있는 곳이면 이동
+            if (cellData != null && cellData.Passable)
+            {   
+                //옮기려는 곳으로 위치 설정 및 플레이어의 위치 변경
+                MoveTo(newCellTarget);
+            }
+            
+        }
+
     }
+
+
 }
